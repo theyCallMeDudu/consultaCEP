290d314338f3e6d981618c2b13dcadb97d7ad972
Eduardo Coelho <coelho.edu96@gmail.com>
2023-05-15 09:18:37 -0300

    Ajustes de validações e ajustes css
    . Validações de input via javascript;
    . Ajustes de media screen css.

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-async function buscaEndereco(cep) {
-    var mensagemErro = document.getElementById('erro');
+var mensagemErro = document.getElementById('erro');
+
+document.addEventListener('DOMContentLoaded', function() {
+    var inputCEP = document.getElementById('cep');
+    
+    inputCEP.addEventListener('input', function() {
+        var valorDigitado = this.value;
+        var valorNumerico = valorDigitado.replace(/[^0-9]/g, '');
+        this.value = valorNumerico;
+        
+        if (valorDigitado.length === 8 && valorDigitado.length > 0) {
+            mensagemErro.innerHTML = "";
+            buscaEndereco(valorNumerico);
+        } else {
+            mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente!</p>`;
+        }
+        
+        if (valorDigitado.length === 0) {
+            mensagemErro.innerHTML = "";
+        }
+    });
+});
+
+async function buscaEndereco(cep) {    
     mensagemErro.innerHTML = "";
+
+    var estado = document.getElementById('estado');
+    var cidade = document.getElementById('cidade');
+    var bairro = document.getElementById('bairro');
+    var logradouro = document.getElementById('endereco');
+    var complemento = document.getElementById('complemento');
 
     try {
         var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
@@ -9,28 +37,26 @@ async function buscaEndereco(cep) {
         if (consultaCEPConvertida.erro) {
             throw Error('O CEP informado não existe!');
         }
-        var estado = document.getElementById('estado');
-        var cidade = document.getElementById('cidade');
-        var bairro = document.getElementById('bairro');
-        var logradouro = document.getElementById('endereco');
 
         estado.innerHTML = consultaCEPConvertida.uf;
         cidade.innerHTML = consultaCEPConvertida.localidade;
         bairro.innerHTML = consultaCEPConvertida.bairro;
         logradouro.innerHTML = consultaCEPConvertida.logradouro;
+        complemento.innerHTML = consultaCEPConvertida.complemento;
 
         console.log(consultaCEPConvertida);
         return consultaCEPConvertida;
     } catch (erro) {
-        mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente!</p>`;
+        mensagemErro.innerHTML = `<p>O CEP informado não existe! Tente novamente!</p>`;
         estado.innerHTML = "";
         cidade.innerHTML = "";
         bairro.innerHTML = "";
         logradouro.innerHTML = "";
-        console.log(erro);
+        complemento.innerHTML = "";
+        console.log(cep, erro, consultaCEP, consultaCEPConvertida);
     }
 }
 
-var cep = document.getElementById('cep');
-console.log(cep);
-cep.addEventListener("focusout", () => buscaEndereco(cep.value));
+// var cep = document.getElementById('cep');
+// console.log(cep);
+// cep.addEventListener("focusout", () => buscaEndereco(cep.value));
